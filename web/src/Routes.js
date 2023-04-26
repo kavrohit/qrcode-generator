d75dd8f8a94f40b7198df2dc1933c47c45fbfde5
rohitAniLabs <rohit@anilabsinc.com>
2023-04-26 17:56:30 +0530

    changed-routes

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -3,7 +3,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/" page={HomePage} name="home" prerender />
+      <Route path="/" page={HomePage} name="home" />
       {/* <Route path="/qrcode/{id:Int}" page={QrCodePage} name="qrCode" /> */}
     </Router>
   )
